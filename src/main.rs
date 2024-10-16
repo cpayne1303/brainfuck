d6649ff73b4c3dfabf201cab7456ffa2b3ec2bb9
connor payne <cpayne1303@gmail.com>
2024-10-16 09:35:49 +0100

    major simplifications

--- src/main.rs
+++ src/main.rs
@@ -26,21 +26,32 @@ impl ByteCodeInterpreter {
         while symbol_num < self.instructions.instructions.len() {
             // println!("{symbol_num}");
             match &self.instructions.instructions[symbol_num] {
-                Instruction::Memory(memory_instruction) => {
-                    match memory_instruction.instruction_type {
-                        Type::Add => {
-                            match memory_instruction.operand {
-                                Option::Some(val) => {
+                Instruction::Memory(operand) => {
+			match operand {
+			Option::Some(val) => {
                                     self.tape[self.tape_pointer] += val;
-                                }
+                                },
                                 _ => {
                                     self.tape[self.tape_pointer] += 1;
-                                }
-                            }
+				}
+                                };
                             symbol_num += 1;
                             continue;
-                        }
-                        Type::Loop => {
+			}
+                Instruction::Pointer(operand) => {
+			match operand {
+                                Option::Some(val) => {
+                                    self.tape_pointer += val;
+                                }
+                                _ => {
+                                    self.tape_pointer += 1;
+                            }
+		    };
+                            symbol_num += 1;
+                            continue;
+		    }
+                        
+                        Instruction::Loop => {
                             if self.tape[self.tape_pointer] == 0 {
                                 symbol_num = matches.get(&symbol_num).unwrap() + 1;
                                 continue;
@@ -50,7 +61,7 @@ impl ByteCodeInterpreter {
                                 continue;
                             }
                         }
-                        Type::LoopEnd => {
+                        Instruction::LoopEnd => {
                             if self.tape[self.tape_pointer] > 0 {
                                 let tmp = *stack.last().unwrap();
                                 symbol_num = tmp + 1;
@@ -61,7 +72,7 @@ impl ByteCodeInterpreter {
                                 continue;
                             }
                         }
-                        Type::Input => {
+                        Instruction::Input => {
                             let mut buffer = [0u8; 1];
                             let _ = std::io::stdin().read_exact(&mut buffer);
                             num = buffer[0];
@@ -69,35 +80,17 @@ impl ByteCodeInterpreter {
                             symbol_num += 1;
                             continue;
                         }
-                        Type::Output => {
+                        Instruction::Output => {
                             let thing = self.tape[self.tape_pointer] as char;
                             print!("{thing}");
                             symbol_num += 1;
                             continue;
                         }
-                        _ => {}
-                    }
-                }
-                Instruction::Pointer(pointer_instruction) => {
-                    match pointer_instruction.instruction_type {
-                        Type::AddPointer => {
-                            match pointer_instruction.operand {
-                                Option::Some(val) => {
-                                    self.tape_pointer += val;
-                                }
-                                _ => {
-                                    self.tape_pointer += 1;
-                                }
-                            }
-                            symbol_num += 1;
-                            continue;
-                        }
-                        _ => {}
-                    }
-                }
+		}
+
+                    };
             }
-        }
-    }
+
 }
 #[derive(Clone)]
 struct ByteCodeObject {
@@ -124,107 +117,72 @@ impl ByteCodeObject {
         tmp.optimize();
         tmp
     }
-    fn group_add_instructions(&mut self) {
+        fn group_add_instructions(&mut self) {
+        let mut instructions: Vec<Instruction> = Vec::new();
+        let mut i = 0;
+        while i < self.instructions.len() {
+            let mut instruction = self.instructions[i].clone();
+            if let Instruction::Memory(ref mut operand) = instruction {
+		    match operand {
+                        Option::Some(ref mut val) => {
+                            while i < self.instructions.len() - 1 {
+                                if let Instruction::Memory(operand2) = &self.instructions[i + 1] {
+                                        i += 1;
+                                        match operand2 {
+                                            Option::Some(val2) => {
+                                                *val = *val + val2;
+                                            }
+					    _ => {}
+                                        };
+                                } else {
+                                    break;
+                                }
+		}
+				}
+				_ => {}
+                };
+	}
+            instructions.push(instruction);
+            i += 1;
+        }
+        self.instructions = instructions;
+    }
+    
+fn group_add_pointer_instructions(&mut self) {
         let mut instructions: Vec<Instruction> = Vec::new();
         let mut i = 0;
         while i < self.instructions.len() {
             // println!("first loop {i}");
             let mut instruction = self.instructions[i].clone();
-            if let Instruction::Memory(ref mut instruction2) = instruction {
-                // println!("instruction is of type Memory");
-                if let Type::Add = instruction2.instruction_type {
-                    // println!("instruction is an Add operation");
-                    match instruction2.operand {
+            if let Instruction::Pointer(ref mut operand) = instruction {
+                    match operand {
                         Option::Some(ref mut val) => {
-                            // println!("the instruction has a valid operand");
-                            // println!("all conditions met");
                             while i < self.instructions.len() - 1 {
-                                // println!("looking at {} inside loop", i+1);
-                                if let Instruction::Memory(instruction3) = &self.instructions[i + 1]
-                                {
-                                    // println!("the next instruction is also a Memory instruction");
-                                    if let Type::Add = instruction3.instruction_type {
-                                        // println!("the next instruction is also of type Add");
+                                if let Instruction::Pointer(operand2) =
+                                    &self.instructions[i + 1] {
                                         i += 1;
-                                        match instruction3.operand {
+                                        match operand2 {
                                             Option::Some(val2) => {
-                                                // println!("the next instruction also has a valid operand");
                                                 *val = *val + val2;
-                                                // println!("{val}");
                                             }
                                             _ => {}
-                                        }
+                                        };
                                     } else {
-                                        // println!("the next instruction is not of type Add");
                                         break;
                                     }
-                                } else {
-                                    // println!("the next instruction is is not a Memory instruction");
-                                    break;
-                                }
-                            }
-                        }
+			    }
+
+			}
                         _ => {}
-                    }
-                }
             }
-            let instruction4 = instruction.clone();
-            instructions.push(instruction4);
+    }
+            instructions.push(instruction);
             i += 1;
         }
         self.instructions = instructions;
     }
-    fn group_add_pointer_instructions(&mut self) {
-        let mut instructions: Vec<Instruction> = Vec::new();
-        let mut i = 0;
-        while i < self.instructions.len() {
-            // println!("first loop {i}");
-            let mut instruction = self.instructions[i].clone();
-            if let Instruction::Pointer(ref mut instruction2) = instruction {
-                // println!("instruction is of type pointer");
-                if let Type::AddPointer = instruction2.instruction_type {
-                    // 	println!("instruction is an addpointer operation");
-                    match instruction2.operand {
-                        Option::Some(ref mut val) => {
-                            // println!("the instruction has a valid operand");
-                            // println!("all conditions met");
-                            while i < self.instructions.len() - 1 {
-                                // println!("looking at {} inside loop", i+1);
-                                if let Instruction::Pointer(instruction3) =
-                                    &self.instructions[i + 1]
-                                {
-                                    // println!("the next instruction is also a pointer instruction");
-                                    if let Type::AddPointer = instruction3.instruction_type {
-                                        // println!("the next instruction is also of type addpointer");
-                                        i += 1;
-                                        match instruction3.operand {
-                                            Option::Some(val2) => {
-                                                // println!("the next instruction also has a valid operand");
-                                                *val = *val + val2;
-                                                // 	println!("{val}");
-                                            }
-                                            _ => {}
-                                        }
-                                    } else {
-                                        // println!("the next instruction is not of type addpointer");
-                                        break;
-                                    }
-                                } else {
-                                    // 					println!("the next instruction is is not a pointer instruction");
-                                    break;
-                                }
-                            }
-                        }
-                        _ => {}
-                    }
-                }
-            }
-            let mut instruction4 = instruction.clone();
-            instructions.push(instruction4);
-            i += 1;
-        }
-        self.instructions = instructions;
-    }
+
+
     fn optimize(&mut self) {
         self.group_add_instructions();
         self.group_add_pointer_instructions();
@@ -232,72 +190,41 @@ impl ByteCodeObject {
     }
 }
 #[derive(Clone)]
-enum Type {
-    Add,
-    AddPointer,
+struct MemoryInstruction {
+    operand: Option<u8>,
+}
+#[derive(Clone)]
+struct PointerInstruction {
+    operand: Option<usize>,
+}
+#[derive(Clone)]
+enum Instruction {
+    Memory(Option<u8>),
+    Pointer(Option<usize>),
     Loop,
     LoopEnd,
     Input,
     Output,
 }
-#[derive(Clone)]
-struct MemoryInstruction {
-    instruction_type: Type,
-    operand: Option<u8>,
-}
-#[derive(Clone)]
-struct PointerInstruction {
-    instruction_type: Type,
-    operand: Option<usize>,
-}
-#[derive(Clone)]
-enum Instruction {
-    Memory(MemoryInstruction),
-    Pointer(PointerInstruction),
-}
 impl Instruction {
     fn add(num: Option<u8>) -> Instruction {
-        let instruction_type = Type::Add;
-        Instruction::Memory(MemoryInstruction {
-            instruction_type,
-            operand: num,
-        })
+        Instruction::Memory(num)
     }
     fn loop_start() -> Instruction {
-        let instruction_type = Type::Loop;
-        Instruction::Memory(MemoryInstruction {
-            instruction_type,
-            operand: Option::None,
-        })
+        Instruction::Loop
     }
     fn loop_end() -> Instruction {
-        let instruction_type = Type::LoopEnd;
-        Instruction::Memory(MemoryInstruction {
-            instruction_type,
-            operand: Option::None,
-        })
+        Instruction::LoopEnd
     }
     fn input() -> Instruction {
-        let instruction_type = Type::Input;
-        Instruction::Memory(MemoryInstruction {
-            instruction_type,
-            operand: Option::None,
-        })
+        Instruction::Input
     }
     fn output() -> Instruction {
-        let instruction_type = Type::Output;
-        Instruction::Memory(MemoryInstruction {
-            instruction_type,
-            operand: Option::None,
-        })
+        Instruction::Output
     }
     fn add_pointer(num: Option<usize>) -> Instruction {
-        let instruction_type = Type::AddPointer;
-        Instruction::Pointer(PointerInstruction {
-            instruction_type,
-            operand: num,
-        })
-    }
+        Instruction::Pointer(num)
+}
 }
 fn cleanup(program: &Vec<char>) -> Vec<char> {
     let instructions = ['+', '-', '<', '>', '[', ']', ',', '.'];
@@ -313,29 +240,25 @@ fn find_matching(data: &ByteCodeObject, symbol_num: usize) -> usize {
     let mut symbol_num2 = symbol_num;
     while right < left {
         symbol_num2 += 1;
-        if let Instruction::Memory(instruction) = &data.instructions[symbol_num2] {
-            match instruction.instruction_type {
-                Type::Loop => {
+        match &data.instructions[symbol_num2] {
+                Instruction::Loop => {
                     left += 1;
                 }
-                Type::LoopEnd => {
+                Instruction::LoopEnd => {
                     right += 1;
                 }
                 _ => {}
             }
         }
-    }
     symbol_num2
 }
 fn get_matches(data: &ByteCodeObject) -> HashMap<usize, usize> {
     let mut matches: HashMap<usize, usize> = HashMap::new();
     for (i, v) in data.instructions.iter().enumerate() {
-        if let Instruction::Memory(v2) = v {
-            if let Type::Loop = v2.instruction_type {
+            if let Instruction::Loop = v {
                 let matching = find_matching(data, i);
                 matches.insert(i, matching);
             }
-        }
     }
     matches
 }
@@ -345,7 +268,7 @@ fn read_program(filename: &str) -> Vec<char> {
 }
 fn main() {
     let st = Instant::now();
-    let program = read_program("mandelbrot.b");
+    let program = read_program("collatz.b");
     let bytecode_object = ByteCodeObject::new(&program);
     let mut bytecode_interpreter = ByteCodeInterpreter::new(bytecode_object);
     bytecode_interpreter.execute_program();
