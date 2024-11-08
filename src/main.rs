93cd4b54b2b199c8c2b3109b8275bfcded58b081
connor payne <cpayne1303@gmail.com>
2024-11-08 21:47:10 +0000

    added command line options

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,40 @@
-use brainfuck2::*;
+use clap::{Parser, ArgAction};
+use brainfuck::*;
 use std::time::Instant;
+#[derive(Parser)]
+#[command(name = "brainfuck_interpreter")]
+#[command(about = "brainfuck interpreter")]
+struct Cli {
+	#[arg()]
+	file_name: Option<String>,
+	#[arg(short = 'c', long)]
+	brainfuck_code: Option<String>,
+	#[arg(short, long= "time")]
+	time: bool,
+}
 fn main() {
-    let st = Instant::now();
-    let bytecode_object = ByteCodeObject::from_file("mandelbrot.b");
+	let cli = Cli::parse();
+	let bytecode_object = if let Some(file_name) = cli.file_name {
+		ByteCodeObject::from_file(&file_name)
+	}
+	else if let Some(brainfuck_code) = cli.brainfuck_code { 
+		let tmp = brainfuck_code.chars().collect::<Vec<char>>();
+		ByteCodeObject::new(&tmp)
+	}
+	else {
+		eprintln!("error");
+		return;
+	};
+
     let mut bytecode_interpreter = ByteCodeInterpreter::new(bytecode_object);
+if cli.time {
+    let st = Instant::now();
     bytecode_interpreter.execute_program();
     let en = st.elapsed();
     println!("{en:?}");
+}
+else {
+    bytecode_interpreter.execute_program();
+
+}
 }
